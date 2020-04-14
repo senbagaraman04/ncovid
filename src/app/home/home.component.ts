c0ffa3065197b0598a7024f25c460036f88088f5
senbagaraman <senbagaraman04 [at] gmail.com>
2020-04-14 17:17:18 +0530

    Last updated time and page refresh updation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../services/http.service';
 import {CovidData, Statewise, CasesTimeSeries} from '../services/covidinterface.service';
+import { Subscription, timer } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
 
 
 
@@ -15,21 +18,19 @@ export class HomeComponent implements OnInit {
   country: Statewise;
   timeSeriesStates: CasesTimeSeries[];
   states: Statewise[];
+  subscription: Subscription;
+  statusText: string;
 
   constructor(private httpService:HttpService) { }
 
   ngOnInit(): void {
-    this.loadData();
-  }
-  loadData() {
-  this.httpService.getfullData().subscribe( (response) => {
-    console.log(response)
-      this.country = response.statewise[0];
-      this.timeSeriesStates = response.cases_time_series;
-      this.states =response.statewise.slice(1);
-  }
 
-  )
+    this.subscription = timer(0, 10000).pipe(switchMap(() => this.httpService.getfullData())).subscribe(response => 
+      {
+        this.country = response.statewise[0];
+        this.timeSeriesStates = response.cases_time_series;
+        this.states =response.statewise.slice(1);
+        
+      });    
   }
-
 }
