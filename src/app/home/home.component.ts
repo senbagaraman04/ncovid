f6b89c9617a0b0377dbfe4c80b7ea999f99551b7
senbagaraman <senbagaraman04 [at] gmail.com>
2020-04-19 09:51:29 +0530

    Added Master table for all states

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,13 @@ import { HttpService } from '../services/http.service';
 import {CovidData, Statewise, CasesTimeSeries} from '../services/covidinterface.service';
 import { Subscription, timer } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
-
+import { HttpClient } from '@angular/common/http';
+import { ViewChild, AfterViewInit} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {merge, Observable, of as observableOf} from 'rxjs';
+import {catchError, map, startWith} from 'rxjs/operators';
+import { MatTableDataSource } from '@angular/material/table';
 
 
 
@@ -14,14 +20,14 @@ import { switchMap } from 'rxjs/operators';
 })
 
 
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
   country: Statewise;
   timeSeriesStates: CasesTimeSeries[];
   states: Statewise[];
   subscription: Subscription;
   statusText: string;
 
-  constructor(private httpService:HttpService) { }
+  constructor(private httpService:HttpService,private _httpClient: HttpClient) { }
 
   ngOnInit(): void {
 
@@ -30,6 +36,20 @@ export class HomeComponent implements OnInit {
         this.country = response.statewise[0];
         this.timeSeriesStates = response.cases_time_series;
         this.states =response.statewise.slice(1);
+        console.log(this.states)
+        this.data = this.states;
       });    
   }
+
+
+  displayedColumns: string[] = ['State','Recovered','Confirmed','Deaths','Active'];
+  data = this.states;
+
+
+  ngAfterViewInit() {
+   this.data = this.states;
+  }
+
 }
+
+
