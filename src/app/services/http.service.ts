c2e6de3d8ba7d986ebb7ca957ef46fbae135a248
Senbagaraman Manoharan <senbagaraman04@gmail.com>
2021-10-08 09:46:36 +0530

    fix: updated httpservice (#20)
    * fix: updated httpservice
    
    * #16_api not working
    
    Co-authored-by: senbagaraman <senbagaraman04 [at] gmail.com>

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { retry } from 'rxjs/operators';
 import { CovidData } from './covidinterface.service';
-
+/**
+ * HttpService service  call for api calls
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -14,32 +15,31 @@ export class HttpService {
   private covid19ApiURL = 'https://data.covid19india.org/';
 
 
+  /**
+   * @returns returns full data in json format
+   */
   getfullData(): Observable<CovidData> {
-
-    return this.http.get<any>(this.covid19ApiURL + 'data.json').pipe(
-      retry(2)
-    );
-
+    return this.http.get<any>(this.covid19ApiURL + 'data.json');
   }
 
-
+  /**
+   * @returns state data fully
+   */
   getStateData(): Observable<any> {
-    return this.http.get<any>(this.covid19ApiURL + 'v2/state_district_wise.json').pipe(
-      retry(2)
-    );
+    return this.http.get<any>(this.covid19ApiURL + 'v2/state_district_wise.json');
   }
 
-
+  /**
+   * @param selectedDate particular date to be selected
+   * @returns data for the selected date
+   */
   getFullDataonDate(selectedDate): Observable<any> {
-    return this.http.get<any>(this.covid19ApiURL + 'v3/min/data-' + selectedDate + '.min.json').pipe(
-      retry(2)
-    );
+    return this.http.get<any>(this.covid19ApiURL + 'v3/min/data-' + selectedDate + '.min.json');
   }
-
 
 
   getStateWiseData(): Observable<any> {
-    return this.http.get<any>(this.covid19ApiURL + 'v3/min/data.min.json').pipe(retry(2));
+    return this.http.get<any>(this.covid19ApiURL + 'v4/min/data.min.json');
   }
 
 }
